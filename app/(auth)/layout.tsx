7db783983aea17cc12ca26b7d268d11dbcf0c1ce
Arthur Laroya <arthur.j.laroya@gmail.com>
2025-01-13 13:28:24 +1100

    feat: add layout and sign-in page components, implement book card and list functionality

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,10 @@
+import React, { ReactNode } from 'react'
+
+const Layout = ({ children } : { children: ReactNode }) => {
+  return (
+    <div>{children}</div>
+    // 1:10:38
+  )
+}
+
+export default Layout
